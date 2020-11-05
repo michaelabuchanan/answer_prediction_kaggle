57b7268d99a52db3e8c1a474f3cbceb187dbc55f
michaelabuchanan <buchanam@oregonstate.edu>
2020-11-05 09:44:14 -0800

    import fixing

--- start_nb.py
+++ start_nb.py
@@ -1,5 +1,8 @@
+sys.path.append("/home/michaela/kaggle/answer_prediction_kaggle/riiideducation")
+
 import riiideducation
 env = riiideducation.make_env()
 
 with timer('cuDF'):
     df = cudf.read_csv('../train.csv')
+
